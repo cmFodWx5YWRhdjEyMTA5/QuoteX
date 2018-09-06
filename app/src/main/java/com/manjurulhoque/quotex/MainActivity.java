b22fb3a867f2a7dec09980f3e4edd7f8d704813e
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-06 22:31:35 +0600

    quotes array

--- app/src/main/java/com/manjurulhoque/quotex/MainActivity.java
+++ app/src/main/java/com/manjurulhoque/quotex/MainActivity.java
@@ -2,12 +2,21 @@ package com.manjurulhoque.quotex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewQuote;
+    private ImageButton btnPrevious, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNext = findViewById(R.id.btnNext);
+        btnPrevious = findViewById(R.id.btnPrevious);
+        mTextViewQuote = findViewById(R.id.textViewQuote);
     }
 }
